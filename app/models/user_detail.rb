92acb3c6984dbd82895b437e294d419e4d209a7c
Jason Bush <jasonbushis@gmail.com>
2012-04-01 03:26:40 -0700

    modifies user detail views and models

--- app/models/user_detail.rb
+++ app/models/user_detail.rb
@@ -1,3 +1,7 @@
 class UserDetail < ActiveRecord::Base
   attr_accessible :address_list_id, :alt_email, :dob, :first_name, :last_name, :middle_name, :phone_list_id, :social, :user_id
+
+belongs_to => :user
+
 end
+
